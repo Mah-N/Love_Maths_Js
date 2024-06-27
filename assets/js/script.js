abf8d484bd4c95c243498a8700c6ef096a51d8dd
Nisha Mahajan <nisha.mah25@gmail.com>
2024-06-27 20:30:40 +0000

    Calculating the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    runGame("addition")
+    runGame("addition");
 
 });
 
@@ -28,13 +28,13 @@ function runGame(gameType) {
      // Creates two random numbers between 1 and 25
      let num1 = Math.floor(Math.random() * 25) + 1;
      let num2 = Math.floor(Math.random() * 25) + 1;
-
+ 
      if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
+         displayAdditionQuestion(num1, num2);
+     } else {
+         alert(`Unknown game type: ${gameType}`);
+         throw `Unknown game type: ${gameType}. Aborting!`;
+     }
 
 }
 
@@ -42,7 +42,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -60,7 +75,6 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
     
-
 }
 
 function displaySubtractQuestion() {
